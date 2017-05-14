6e70b073adbf97b65d8719975fc3fe01374049f1
Sergey <ushakovsr9ey@gmail>
2017-05-14 13:14:29 +0400

    Improvement graphics

--- src/com/huorehu/battlecity/display/Display.java
+++ src/com/huorehu/battlecity/display/Display.java
@@ -4,6 +4,9 @@ import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+import java.util.Arrays;
 
 import javax.swing.JFrame;
 
@@ -13,20 +16,23 @@ public abstract class Display {
     private static JFrame window;
     private static Canvas content;
 
-    public static void create(int width, int height, String title) {
+    private static BufferedImage buffer;
+    private static int[] bufferData;
+    private static Graphics bufferGraphics;
+    private static int clearColor;
+
+    // temp
+    private static float delta = 0;
+    // endTemp
+
+    public static void create(int width, int height, String title, int _clearColor) {
         if (created) {
             return;
         }
 
         window = new JFrame(title);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        content = new Canvas() {
-            @Override
-            public void paint(Graphics g) {
-                super.paint(g);
-                render(g);
-            }
-        };
+        content = new Canvas();
 
         Dimension size = new Dimension(width, height);
         content.setPreferredSize(size);
@@ -36,16 +42,27 @@ public abstract class Display {
         window.pack();
         window.setLocationRelativeTo(null);
         window.setVisible(true);
+
+        buffer = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        bufferData = ((DataBufferInt) buffer.getRaster().getDataBuffer()).getData();
+        bufferGraphics = buffer.getGraphics();
+        clearColor = _clearColor;
+
+        created = true;
+    }
+
+    public static void clear() {
+        Arrays.fill(bufferData, clearColor);
     }
 
     public static void render() {
-        content.repaint();
-        content.setBackground(Color.blue);
+        bufferGraphics.setColor(new Color(0xff0000ff));
+        bufferGraphics.fillOval((int) (350 + Math.sin(delta) * 200), 250, 100, 100);
+        delta += 0.02;
     }
 
-    private static void render(Graphics g) {
-        g.setColor(Color.GREEN);
-        g.fillOval(400 - 50, 300 - 50, 100, 25);
+    public static void swapBuffers() {
+        Graphics g = content.getGraphics();
+        g.drawImage(buffer, 0, 0, null);
     }
-
 }
--- src/com/huorehu/battlecity/main/Main.java
+++ src/com/huorehu/battlecity/main/Main.java
@@ -10,14 +10,15 @@ import com.huorehu.battlecity.display.Display;
 public class Main {
 
     public static void main(String[] args) {
-        Display.create(800, 600, "Tanks");
+        Display.create(800, 600, "Tanks", 0xff00ff00);
 
         Timer timer = new Timer(1000 / 60, new AbstractAction() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
+                Display.clear();
                 Display.render();
-
+                Display.swapBuffers();
             }
         });
 
