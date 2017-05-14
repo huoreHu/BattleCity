0f9f0cde23659b5f028c6ccca6965e7a40b8aa28
Sergey <ushakovsr9ey@gmail>
2017-05-14 09:22:15 +0400

    added main window

---
+++ src/com/huorehu/battlecity/main/Main.java
@@ -0,0 +1,11 @@
+package com.huorehu.battlecity.main;
+
+import com.huorehu.battlecity.display.Display;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Display.create(800, 600, "Tanks");
+    }
+
+}
